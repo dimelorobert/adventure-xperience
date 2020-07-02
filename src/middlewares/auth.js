44c75cbe5330c6bbd9c7b32381bd94c41d4d45d5
Robert Hernandez Arenas <52069820+dimelorobert@users.noreply.github.com>
2020-07-02 18:36:42 +0200

    Arreglado error auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const {
-   SECRET_KEY
+    SECRET_KEY
 } = process.env;
 
 const jwt = require('jsonwebtoken');
 
 const {
-   getConnection
+    getConnection
 } = require('../database');
 const {
-   helpers
+    helpers
 } = require('../helpers');
 
 let connection;
@@ -17,66 +17,68 @@ let connection;
 
 async function userAuthenticated(request, response, next) {
 
-   try {
-      connection = await getConnection();
-      const {
-         authorization
-      } = request.headers;
-      let decoded;
-      try {
-         decoded = jwt.verify(authorization, SECRET_KEY);
-         console.log(authorization);
-      } catch (error) {
-         return response.status(500).json({
-            status: 'error',
-            code: 500,
-            error: `El token no esta bien formado`
-         });;
-      }
+    try {
+        connection = await getConnection();
+        const {
+            authorization
+        } = request.headers;
+        let decoded;
+        try {
+            decoded = jwt.verify(authorization, SECRET_KEY);
+            console.log(authorization);
+        } catch (error) {
+            return response.status(500).json({
+                status: 'error',
+                code: 500,
+                error: `El token no esta bien formado`
+            });
+        }
 
-      const {
-         id,
-         iat
-      } = decoded;
-      const [result] = connection.query(`
+        const {
+            id,
+            iat
+        } = decoded;
+        const [result] = connection.query(`
          SELECT last_password_update FROM user WHERE id=?`, [id]);
 
-      if (!result.length) {
-         return response.status(404).json({
-            status: 'error',
-            code: 404,
-            error: `El usuario con el id ${user.id} no existe`
-         });
-      };
-      const [user] = result;
+        if (!result.length) {
+            return response.status(404).json({
+                status: 'error',
+                code: 404,
+                error: `El usuario con el id ${user.id} no existe`
+            });
+        };
+        const [user] = result;
 
-      if (new Date(iat * 1000) < new Date(user.last_password_update)) {
-         return response.status(404).json({
-            status: 'error',
-            code: 404,
-            error: `El token ha expirado, inicia sesión para renovarlo`
-         });
-      };
-      request.authorization = decoded
-      next();
-   } catch (error) {
-      error.http = 401;
-      next(error);
-   } finally {
-      if (connection) {
-         connection.release();
-      }
-   }
+        if (new Date(iat * 1000) < new Date(user.last_password_update)) {
+            return response.status(404).json({
+                status: 'error',
+                code: 404,
+                error: `El token ha expirado, inicia sesión para renovarlo`
+            });
+        }
+        request.authorization = decoded
+        next();
+    } catch (error) {
+        error.http = 401;
+        next(error);
+    } finally {
+        if (connection) {
+            connection.release();
+        }
+    }
 
-   function userIsAdmin(request, response, next) {
-      if (!request.authorization || request.authorization.role !== 'admin') {
-         helpers.errorGenerator(('No tienes privilegios de administrador'), 401);
-         return next(error);
-      }
-   }
-   next();
+
 }
 
-module.exports = {
-   userAuthenticated
-};
+function userIsAdmin(request, response, next) {
+    if (!request.authorization || request.authorization.role !== 'admin') {
+        const error = new Error('No tienes privilegios de administración');
+        error.httpCode = 401;
+        return next(error);
+    }
+
+    next();
+
+}
+module.exports = { userAuthenticated, userIsAdmin }
