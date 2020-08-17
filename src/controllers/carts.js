6aaa8c2d88b8ce4d2667e44e774d1505f6c7c606
Robert Hernandez <airbusjayrobert@gmail.com>
2020-08-17 11:48:02 +0200

    Terminado controller cart/create que se encarga de gestionar las reservas o las compras de la app

--- src/controllers/carts.js
+++ src/controllers/carts.js
@@ -4,8 +4,6 @@
 require('dotenv').config();
 const {
    PUBLIC_HOST,
-   ADVENTURE_VIEW_UPLOADS,
-   ADVENTURE_UPLOADS_DIR,
    SECRET_KEY,
    SERVICE_EMAIL,
    ADMIN_EMAIL,
@@ -15,17 +13,9 @@ const {
    getConnection
 } = require('../database');
 const {
-   adventuresSchema
-} = require('../validations');
-const {
    helpers
 } = require('../helpers');
-const path = require('path');
-const jwt = require('jsonwebtoken');
-let dateNow = helpers.formatDateToDB(new Date());
 const nodemailer = require('nodemailer');
-let creating_date = helpers.formatDateJSON(new Date());
-let adventureImagePath = path.join(__dirname, `../${ADVENTURE_UPLOADS_DIR}`);
 let connection;
 
 const cartsController = {
@@ -42,12 +32,17 @@ const cartsController = {
          } = request.authorization;
          console.log(tokenPayload);
          const {
-            id
+            id,
+            email
          } = tokenPayload;
 
+         const [user] = await connection.query(`
+            SELECT name, surname
+            FROM users 
+            WHERE id=?`, [id]);
 
          const [adventureSelected] = await connection.query(`
-            SELECT name, country, city 
+            SELECT name, country, city , start_date_event 
             FROM adventures 
             WHERE id=?`, [adventure_id]);
 
@@ -63,7 +58,8 @@ const cartsController = {
          const {
             name,
             country,
-            city
+            city,
+            start_date_event
          } = destructuringNameCountryCity;
 
          await connection.query(`
@@ -71,18 +67,19 @@ const cartsController = {
             SET date_selected=CURRENT_TIMESTAMP() 
             WHERE id=?;`, [adventure_id]);
 
+         let booking;
 
          if (paid === 'reservado') {
-            const [newBooking] = await connection.query(`
+            [booking] = await connection.query(`
                INSERT INTO cart(purchased_date, date_booking, total_price, paid, user_id, adventure_id)
                VALUES(null, CURRENT_TIMESTAMP(), ?, 'reservado', ?, ? );`,
                [total_price, id, adventure_id]);
 
          } else if (paid === 'pagado') {
-            const [newBooking] = await connection.query(`
-            INSERT INTO cart(purchased_date, date_booking, total_price, paid, user_id, adventure_id)
-            VALUES(CURRENT_TIMESTAMP(), null,  ?, 'reservado', ?, ? );
-            `, [total_price, id, adventure_id]);
+            [booking] = await connection.query(`
+               INSERT INTO cart(purchased_date, date_booking, total_price, paid, user_id, adventure_id)
+               VALUES(CURRENT_TIMESTAMP(), null,  ?, 'pagado', ?, ? );`,
+               [total_price, id, adventure_id]);
 
          } else {
             return response.status(404).json({
@@ -91,6 +88,68 @@ const cartsController = {
                error: 'No ha sido posible efectuar la reserva o la compra de la aventura seleccionada'
             });
          }
+
+         // we check if the email exist into db
+         const [existingEmail] = await connection.query(`SELECT email FROM users WHERE id=?`, [id]);
+
+         if (existingEmail.length) {
+            try {
+               const transporter = nodemailer.createTransport({
+                  service: SERVICE_EMAIL,
+                  auth: {
+                     user: ADMIN_EMAIL,
+                     pass: PASSWORD_ADMIN_EMAIL
+                  }
+               });
+               const mailOptions = {
+                  from: ADMIN_EMAIL,
+                  to: `${email}`,
+                  subject: `Nueva compra o reserva Aventura Xperience`,
+                  text: `Este email es para confirmar tu reserva o tu compra realizada en la web aventura Xperience`,
+                  html: `
+            <div>
+              <h1> Ticket de Reserva o compra Nº.ticket: ${booking.insertId} </h1>
+              <p> Hola ${user[0].name} ${user[0].surname}:</p>
+              <p> Haz efectuado la reserva o la compra de la siguiente aventura</p>
+              <h2>Datos de la aventura creada: </h2>
+              <br>
+              <spa> Id de la aventura: ${adventure_id}</spa>
+              <br> 
+              <spa> Id usuario: ${id}</spa>
+              <br>
+              <spa> nombre: ${name}</spa>
+              <br>
+              <spa> Precio: ${total_price}</spa>
+              <br>
+              <spa> País - Ciudad: ${country} - ${city}</spa>
+              <br>
+              <spa> Fecha del evento: ${start_date_event}</spa>
+              <br>
+            </div>`
+               };
+
+               transporter.sendMail(mailOptions, (error, info) => {
+
+                  response.status(200).json(request.body);
+
+               });
+
+            } catch (error) {
+               console.log(error);
+               if (error) {
+                  response.status(500).send(error.message);
+               }
+            }
+
+         } else {
+            return response.status(500).json({
+               status: 'error',
+               code: 500,
+               error: `No se pudo enviar el email debido a un error en el servidor`
+            });
+         }
+
+
          response.send({
             status: 200,
             data: {
