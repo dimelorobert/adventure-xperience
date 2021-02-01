2a103e793e86005f77cb3bb3e1bb256c303ad16f
DimeloRobert <airbusjayrobert@gmail.com>
2021-02-01 16:33:54 +0100

    :hammer: (registerSchema.js): corregido el catcheo del error trim

--- src/components/users/validations/registerSchema.js
+++ src/components/users/validations/registerSchema.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const registerSchema = Joi.object().keys({
-  email: Joi.string().email().required().messages({
+  email: Joi.string().email().required().trim().messages({
     "string.email": "Debes introducir un email valido",
     "string.required": "El camppo email es obligatorio",
     "string.trim": "El email no debe contener espacios",
@@ -10,11 +10,13 @@ const registerSchema = Joi.object().keys({
     .min(6)
     .max(16)
     .alphanum()
+    .trim()
     .pattern(new RegExp("^[A-z0-9]{6,16}$"))
     .required()
     .messages({
-      "string.empty": "El campo contraseña no debe estar vacío",
+      "string.empty": "La contraseña no debe estar vacía",
       "string.alphanum": "La contraseña solo permite caracteres alfanúmericos",
+      "string.trim": "La contraseña no debe contener espacios",
       "string.required": "El campo contraseña es obligatorio",
       "string.min": "La contraseña debe tener mínimo 6 cáracteres",
       "string.max": "La contraseña debe tener máximo 16 cáracteres",
